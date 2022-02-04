0e0f2595f1d8e3ded28d2b7b55a0118345bd92e6
matteoorig <matteoorig@gmail.com>
2022-02-04 12:49:53 +0100

    update

--- app/App.js
+++ app/App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Tutorial1 from './pages/Tutorial1';
+import Tutorial2 from './pages/Tutorial2';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <Tutorial1/>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Tutorial1" component={Tutorial1} options={{headerShown: false}}/>
+        <Stack.Screen name="Tutorial2" component={Tutorial2} options={{headerShown: false}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
--- app/pages/Tutorial1.js
+++ app/pages/Tutorial1.js
@@ -1,17 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View , Image, TouchableWithoutFeedback} from 'react-native';
+
 import Page1text from '../components/page1text';
 import SushiClub from '../components/SushiClub';
 
+const navigateToNew = (props) =>{
+    props.navigation.navigate('Tutorial2');
+}
 
-
-const Tutorial1 = ()=>{
+const Tutorial1 = (props)=>{
     return(
         
-        <ImageBackground style={{height:'100%', position:'relative'}} source={require('../img/sushiBack.jpg')}>
-            <View style={{width:'100%', height:'100%',position:'absolute', backgroundColor:'black', opacity:0.5}}></View>
+        <ImageBackground style={{height:'100%', position:'relative', justifyContent:'center', alignItems:'center'}} blurRadius={3} source={require('../img/sushiBack.jpg')}>
+            <View style={{width:'100%', height:'100%',position:'absolute', backgroundColor:'black', opacity:0.8}}></View>
             <SushiClub/>
             <Page1text height={'50%'} width={'100%'}/>
+            <Image source={require('../img/bottoneSushi.png')} style={{width:352,height:200, resizeMode:'contain', position:'relative'}} />
+            <TouchableWithoutFeedback onPress={()=>{navigateToNew(props)}}><View style={{width:150, height:150,position:'absolute', bottom:0, marginBottom:80}}></View></TouchableWithoutFeedback>
         </ImageBackground>
         
     );
