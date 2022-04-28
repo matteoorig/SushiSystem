854a16ad3beb3221155d9f699c2383de27197e1a
EliaSaetti <82151110+EliaSaetti@users.noreply.github.com>
2022-04-28 13:54:51 +0200

    commit

--- AdminPanel/method.js
+++ AdminPanel/method.js
@@ -1,20 +1,40 @@
 //Get tavoli
 //porta -> 8890
 //ip -> 
-function getTavoli()
+
+
+function getTavoli(id)
 {
-    fetch("http://172.16.102.76:8890/sushiSystem/getTavoli").then((data)=>{
-        return data.json(); //Cast in json
-    }).then((res)=>{
-        console.log(res);
+    console.log("object");
+    fetch("http://172.16.102.69:8890/SushiSystem/getTavoli").then((res)=>{
+        return res.json(); //Cast in json
+    }).then((data)=>{
+        console.log(data);
+
+        let tavolo = {
+            "nTavolo": data.value[id].nTavolo,
+            "nomeTavolo": data.value[id].nomeTavolo,
+            "numeroClienti": data.value[id].clienti.length
+        }
+
+        console.log(tavolo);
+        
+        
+
+    }).catch((err)=>{
+        console.log(err);
     })
 }
 
 function getOrdini(id)
 {
-    fetch("http://172.16.102.76:8890/sushiSystem/getOrdini").then((data)=>{
-        return data.json(); //Cast in json
-    }).then((res)=>{
-        console.log(res);
+    fetch("http://172.16.102.69:8890/sushiSystem/getOrdini").then((res)=>{
+        return res.json(); //Cast in json
+    }).then((data)=>{ //data è il contenuto della response (res) già castato in json
+
+        ordini
+        console.log(tavolo);
+
+        console.log(data);
     })
 }
